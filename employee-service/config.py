c70a5a62618a426142e02970a8de6737ddfcbfb1
carladcramos <crlrms28@gmail.com>
2025-03-06 13:36:14 +0800

    completed

--- employee-service/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-DB_USERNAME = "your_username"
-DB_PASSWORD = "your_password"
-DB_HOST = "localhost"
-DB_NAME = "employee_db"
-
-SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
-SQLALCHEMY_TRACK_MODIFICATIONS = False
